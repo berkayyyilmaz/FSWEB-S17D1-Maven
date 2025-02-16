dcdde808e82d6c646e13e6a44fe2e69c259dbc61
berkayyyilmaz <yilmaz.berkayyagiz@gmail.com>
2025-02-16 17:40:37 +0300

    Completed

---
+++ src/main/java/com/workintech/fswebs17d1/entity/Animal.java
@@ -0,0 +1,50 @@
+package com.workintech.fswebs17d1.entity;
+
+import java.util.Objects;
+
+public class Animal {
+
+    private Integer id;
+    private String name;
+
+    public Animal(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(id, animal.id) && Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
